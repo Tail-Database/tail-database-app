dc172fc3b9ec8303f499a5d2c3c84b87e181c61d
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-10-21 12:59:24 +0100

    display TAILs in data grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import logo from './assets/logo-light.png';
 import './App.css';
 
 declare global {
   interface Window {
-    taildatabase? : any
+    taildatabase?: any
   }
 }
 
+type Tail = {
+  hash: string;
+  name: string;
+  code: number;
+};
+
+const columnHelper = createColumnHelper<Tail>();
+
+const columns = [
+  columnHelper.accessor('hash', {
+    header: () => 'TAIL Hash / Asset ID',
+    cell: info => `${info.getValue().slice(0, 20)}...`,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor(row => row.name, {
+    id: 'name',
+    cell: info => <i>{info.getValue()}</i>,
+    header: () => 'Name',
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('code', {
+    header: () => 'Code',
+    cell: info => info.renderValue(),
+    footer: info => info.column.id,
+  }),
+];
+
 function App() {
   const [tails, setTails] = useState([]);
+  const table = useReactTable({
+    data: tails,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   useEffect(() => {
     window.taildatabase.getTails().then((res: any) => setTails(res))
@@ -19,16 +57,36 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <ul>
-            {tails.map((tail: any) => (
-              <li key={tail.hash}>
-                0x{tail.hash} - {tail.name}
-              </li>
-            ))}
-          </ul>
-        </p>
       </header>
+      <table className="tail-table">
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map(row => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </div>
   );
 }
