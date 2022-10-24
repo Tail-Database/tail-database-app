829c1f25a0d5d3dd1ed7ca59d499ef31fe0b2005
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-10-24 19:38:36 +0100

    feedback on status of adding TAIL record

--- src/pages/AddTail.tsx
+++ src/pages/AddTail.tsx
@@ -11,14 +11,12 @@ function AddTail() {
     const [code, setCode] = useState('');
     const [category, setCategory] = useState('');
     const [description, setDescription] = useState('');
-
-    useEffect(() => {
-    }, []);
+    const [inserted, setInserted] = useState(false);
+    const [failed, setFailed] = useState(false);
 
     const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        // Todo: validate hash, name code
         if (hash.length !== 64) {
             console.error('hash.length !== 64');
             return;
@@ -29,7 +27,7 @@ function AddTail() {
             return;
         }
 
-        if (code.length <1 || code.length > 5) {
+        if (code.length < 1 || code.length > 5) {
             console.error('code.length <1 || code.length > 5');
             return;
         }
@@ -42,6 +40,14 @@ function AddTail() {
                 category,
                 description
             });
+
+            if (tx_id) {
+                setInserted(true);
+                setFailed(false);
+            } else {
+                setInserted(false);
+                setFailed(true);
+            }
 
             console.log(hash, name, code, tx_id)
         } catch (err) {
@@ -59,14 +65,24 @@ function AddTail() {
         <Layout>
             <div className="tail-info">
                 <h1>Add CAT TAIL</h1>
-                <form id="form-add-tail" onSubmit={onSubmit}>
-                    <p>Hash: <input type="text" name="hash" onChange={onHashChange} /></p>
-                    <p>Name: <input type="text" name="name" onChange={onNameChange} /></p>
-                    <p>Code: <input type="text" name="code" onChange={onCodeChange} /></p>
-                    <p>Category: <input type="text" name="category" onChange={onCategoryChange} /></p>
-                    <p>Description: <input type="text" name="description" onChange={onDescriptionChange} /></p>
-                    <input type="submit" value="Add TAIL" />
-                </form>
+                {inserted && (
+                    <>TAIL record submitted to mempool</>
+                )}
+                {!inserted && (
+                    <form id="form-add-tail" onSubmit={onSubmit}>
+                        <p>Hash: <input type="text" name="hash" onChange={onHashChange} /></p>
+                        <p>Name: <input type="text" name="name" onChange={onNameChange} /></p>
+                        <p>Code: <input type="text" name="code" onChange={onCodeChange} /></p>
+                        <p>Category: <input type="text" name="category" onChange={onCategoryChange} /></p>
+                        <p>Description: <input type="text" name="description" onChange={onDescriptionChange} /></p>
+                        <input type="submit" value="Add TAIL" />
+                        
+                    </form>
+                )}
+                {failed && (
+                    <>Failed to submit TAIL record to mempool. You can only submit the same TAIL hash once. If you recently submitted a record you must wait for it to clear before submitting another.</>
+                )}
+
             </div>
         </Layout>
     );
