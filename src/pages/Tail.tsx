6fb6d946efd741002f5bc154c79a95fa9d1933d7
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-10-28 12:08:39 +0100

    display eve coin id and clvm reveal

--- src/pages/Tail.tsx
+++ src/pages/Tail.tsx
@@ -5,17 +5,24 @@ import Layout from '../Layout';
 
 function Tail() {
     const { hash } = useParams();
-    const [tail, setTail] = useState({ hash: '', name: '', code: '', category: '', description: '', launcherId: '' });
+    const [tail, setTail] = useState({ hash: '', name: '', code: '', category: '', description: '', launcherId: '', eveCoinId: '' });
     const [logoUrl, setLogoUrl] = useState<string | null>(null);
+    const [tailReveal, setTailReveal] = useState('');
 
     useEffect(() => {
         window.taildatabase.getTail(hash || '')
-            .then((res: any) => {
+            .then(res => {
                 setTail(res);
 
-                return window.taildatabase.getNftUri(res.launcherId);
+                return Promise.all([
+                    window.taildatabase.getTailReveal(res.eveCoinId),
+                    window.taildatabase.getNftUri(res.launcherId)
+                ]);
             })
-            .then((url) => setLogoUrl(url))
+            .then(([[eve_coin_id, _, tail_reveal], url]) => {
+                setLogoUrl(url);
+                setTailReveal(tail_reveal);
+            })
             .catch(console.error)
     }, []);
 
@@ -28,6 +35,8 @@ function Tail() {
                 <p>Code: {tail.code}</p>
                 <p>Category: {tail.category}</p>
                 <p>Description: {tail.description}</p>
+                <p>Eve coin id: {tail.eveCoinId}</p>
+                <p>TAIL reveal: {tailReveal}</p>
                 {logoUrl && (<img src={logoUrl} />)}
             </div>
         </Layout>
