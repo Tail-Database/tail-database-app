478d8977de4c471553e4e88d3a439b53f9a1231c
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-11-02 16:09:21 +0000

    poll to auto update tails list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -51,7 +51,7 @@ const columns = [
   }),
 ];
 
-function HomePage() {
+const Tails = () => {
   const [tails, setTails] = useState([]);
   const table = useReactTable({
     data: tails,
@@ -60,45 +60,57 @@ function HomePage() {
   });
 
   useEffect(() => {
-    window.taildatabase.getTails().then((res: any) => setTails(res))
+    (function getTails() {
+      window.taildatabase.getTails()
+        .then((res: any) => setTails(res))
+        .then(() => {
+          setTimeout(getTails, 2500);
+        });
+    })();
   }, []);
 
+  return (
+    <table className="table">
+      <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map(row => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map(cell => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+function HomePage() {
   return (
     <Layout>
       <div className="row">
         <div className="col-md-12">
-        <table className="table">
-        <thead>
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map(row => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map(cell => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+          <Tails />
         </div>
       </div>
-      
+
     </Layout>
   );
 }
